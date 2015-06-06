6e0e89862f63d08e4fbbfece1afb9eb6bdf073d6
Test <nikhil.jain0987@gmail.com>
2015-06-05 23:00:04 -0400

    User can now load target picture

--- MalluApp/app/src/main/java/com/hello/learning/malluapp/MainActivity.java
+++ MalluApp/app/src/main/java/com/hello/learning/malluapp/MainActivity.java
@@ -1,36 +1,75 @@
 package com.hello.learning.malluapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
 
 
 public class MainActivity extends Activity {
+
+    Button loadPictureButton;
+    Button takePictureButton;
+    ImageView image;
+
+    Bitmap imageBitmap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadPictureButton = (Button) findViewById(R.id.choosePictureButton);
+        takePictureButton = (Button) findViewById(R.id.takePictureButton);
+        image = (ImageView) findViewById(R.id.imageView);
+
+        loadPictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (resultCode == RESULT_OK) {
+
+            Uri imagePath = data.getData();
+            Log.d("path of image: ", imagePath.toString());
+
+            try {
+                imageBitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imagePath));
+                image.setImageBitmap(imageBitmap); // display the pic
+            }
+
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
         }
-        return super.onOptionsItemSelected(item);
+
+
     }
+
+
+
+
 }
